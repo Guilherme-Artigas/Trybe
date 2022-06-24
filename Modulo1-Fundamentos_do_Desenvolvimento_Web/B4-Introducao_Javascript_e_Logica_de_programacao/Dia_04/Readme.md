e031ad17c3569718c9faa6924243998101ef5b4e
Guilherme Artigas <guilherme.artigas92@gmail.com>
2022-06-24 15:21:00 -0300

    Resolução dos exercicios de fixação do 1 ao 5

---
+++ Modulo1-Fundamentos_do_Desenvolvimento_Web/B4-Introducao_Javascript_e_Logica_de_programacao/Dia_04/Readme.md
@@ -0,0 +1,8 @@
+# Dia 04 ☀️
+Dando continuidade nos estudos em lógica com **Javascript** hoje vamos ver algumas estruturas fundamentais da linguagem para o nosso dia a dia como desenvolvedores.
+
+### Lista de assuntos do dia: 📚
+
+- [x] Manipular objetos;
+- [x] Utilizar for/in e for/of;
+- [x] Manipular funções e estruturar o código;
---
+++ Modulo1-Fundamentos_do_Desenvolvimento_Web/B4-Introducao_Javascript_e_Logica_de_programacao/Dia_04/exeFixacao-03.js
@@ -0,0 +1,13 @@
+// 3 - Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo as datas em que a jogadora Marta foi considerada a melhor do mundo.
+
+let player = {
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: {
+    golden: 2,
+    silver: 3,
+  },
+  bestInTheWorld: [2006, 2007, 2008, 2009, 2018],
+};
+console.log(`A jogadora ${player.name} ${player.lastName} tem ${player.age} anos de idade`);
