d50ed0c4ae5d8b0353c82b0c8677a69d4884818e
Guilherme Artigas <guilherme.artigas92@gmail.com>
2022-06-28 09:29:51 -0300

    Resolução dos exz

---
+++ Modulo1-Fundamentos_do_Desenvolvimento_Web/B5-Javascript_DOM_Eventos_WebStorage/Dia_01/Readme.md
@@ -0,0 +1,8 @@
+# Dia 01 🌥️
+
+
+Iniciando o bloco cinco, o assunto de hoje é o **DOM**, entender essa estrutura que faz a ponte entre o **HTML** e o **Javascript** e que permite a interação entre as duas tecnologias.
+
+#### Lista de assuntos do dia: 📔
+
+- [x] Acessar elementos do **HTML** com o **Javascript** através dos seletores do **DOM**
---
+++ Modulo1-Fundamentos_do_Desenvolvimento_Web/B5-Javascript_DOM_Eventos_WebStorage/Dia_01/praticaDaAula/script.js
@@ -0,0 +1,26 @@
+// body
+window.document.getElementsByTagName('body')[0].style.background = '#F3F3F3';
+
+// Cabeçalho
+window.document.querySelector('#header-container').style.background = '#00B069';
+
+// Section 1
+window.document.querySelector('section.emergency-tasks').style.background = '#FF9F84';
+
+// 1º h3 da section 1
+window.document.querySelectorAll('section.emergency-tasks div h3')[0].style.background = '#A500F3';
+
+// 2º h3 da section 1
+window.document.querySelectorAll('section.emergency-tasks div h3')[1].style.background = '#A500F3';
+
+// section 2
+window.document.querySelector('section.no-emergency-tasks').style.background = '#F9DB5E';
+
+// 1º h3 da section 2
+window.document.querySelectorAll('section.no-emergency-tasks div h3')[0].style.background = '#232525';
+
+// 2º h3 da section 2
+window.document.querySelectorAll('section.no-emergency-tasks div h3')[1].style.background = '#232525';
+
+// footer
+window.document.querySelector('footer').style.background = '#003533';
