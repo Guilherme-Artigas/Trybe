3fb66c5cfec8a6a99a2c8369e096e8469f727e26
Guilherme Artigas <guilherme.artigas92@gmail.com>
2022-07-14 19:03:26 -0300

    Exercicio 04 resolvido

---
+++ Modulo1-Fundamentos_do_Desenvolvimento_Web/B7-Introducao_Javascript_ES6_Testes_Unitarios/Dia_01/exercicios/parte2/ex04.js
@@ -0,0 +1,43 @@
+/*
+  Crie duas funções JavaScript com as seguintes especificações:
+
+  Não se esqueça de usar template literals
+
+  Função 1: Escreva uma função que substitua a letra 'x' em uma frase.
+
+    A função deverá receber um nome por parâmetro;
+    Declare dentro da função uma variável com o nome frase, do tipo const e com o valor igual a 'Tryber x aqui!';
+    A função deverá retornar uma nova frase onde o x da frase 'Tryber x aqui!' seja substituído pelo nome passado por parâmetro.
+  
+  Exemplo:
+
+    Parâmetro: 'Bebeto'
+    Retorno: 'Tryber Bebeto aqui!'
+  
+  Função 2: Escreva uma função que irá receber o retorno da Função 1 por parâmetro e retornará uma nova string.
+  
+    A função deverá receber o retorno da Função 1 por parâmetro;
+    Declare dentro da função uma variável com o nome skills, do tipo const;
+      A variável skills deverá ser um array contendo três strings com tecnologias que você já aprendeu.
+    Concatene o valor retornado da Função 1, a frase 'Minhas três principais habilidades são:' e o valor da variável skills.
+
+  Exemplo de retorno:
+
+  Tryber Bebeto aqui!
+  Minhas três principais habilidades são:
+
+    JavaScript
+    HTML
+    CSS
+*/
+const getName = arg1 => `Tryber ${arg1} aqui!`;
+const habilidades = arg1 => {
+  const skills = ['HTML', 'CSS', 'JavaScript'];
+  return `${getName(arg1)}
+  Minhas três habilidades são:
+  
+  ${skills[0]}
+  ${skills[1]}
+  ${skills[2]}`
+};
+console.log(habilidades('Guilherme Artigas'));
