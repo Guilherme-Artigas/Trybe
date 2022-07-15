e38617659dbfc27c97698100f4ba581b70584aea
Guilherme Artigas <guilherme.artigas92@gmail.com>
2022-07-15 13:30:20 -0300

    Exercicios 1, 2 dia 02 Bloco 07

---
+++ Modulo1-Fundamentos_do_Desenvolvimento_Web/B7-Introducao_Javascript_ES6_Testes_Unitarios/Dia_02/exercicios/parte2/ex01.js
@@ -0,0 +1,69 @@
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      }
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      }
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    }
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+const customerInfo = (order) => {
+  // Adicione abaixo as informações necessárias.
+  console.log(`Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, telefone: ${order.phoneNumber}, R. ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}`);
+}
+
+customerInfo(order);
+
+const orderModifier = (order) => {
+  // Adicione abaixo as informações necessárias.
+  const newPerson = Object.assign({}, order.name, order.payment);
+  newPerson.name = 'Luiz Silva';
+  newPerson.payment = 50;
+  const listaPizzas = Object.keys(order.order.pizza);
+  console.log(`Olá ${newPerson.name} o total do seu pedido de ${listaPizzas[0]}, ${listaPizzas[1]} e ${order.order.drinks.coke.type} é R$ ${newPerson.payment},00.`)
+}
+
+orderModifier(order);
+
+
+
+
+// const person = {
+//   name:'Roberto',
+// };
+
+// const lastName = {
+//   lastName: 'Silva',
+// };
+
+// const newPerson = Object.assign({},person,lastName);
+// newPerson.name = 'Gilberto';
+// console.log(newPerson);
+// console.log(person);
