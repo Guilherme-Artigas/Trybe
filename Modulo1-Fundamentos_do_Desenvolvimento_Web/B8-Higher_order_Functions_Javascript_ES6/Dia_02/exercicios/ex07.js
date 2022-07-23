c6255f628749e496c3bc05c85cb6bf8d0f5f3ffc
Guilherme Artigas <guilherme.artigas92@gmail.com>
2022-07-22 23:23:45 -0300

    Exercicios 05, 06, 07 dia 02 Bloco 08 Modulo 01

---
+++ Modulo1-Fundamentos_do_Desenvolvimento_Web/B8-Higher_order_Functions_Javascript_ES6/Dia_02/exercicios/ex07.js
@@ -0,0 +1,77 @@
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+function authorUnique(array) {
+  // escreva seu código aqui
+  const lista = [];
+  array.forEach((e) => lista.push(e.author.birthYear));
+  for (let i1 = 0; i1 < lista.length; i1 += 1) {
+    for (let i2 = i1+1; i2 < lista.length; i2 += 1) {
+      if (lista[i1] === lista[i2]) {
+        return false;
+      };
+    };
+  };
+  return true;
+};
+console.log(authorUnique(books));
